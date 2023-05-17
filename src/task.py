0280756efcac281271c35c45f3d271ea3bd85d9f
maksyuki <maksyuki@126.com>
2023-05-17 20:34:01 +0800

    feat: add schedule module and test

--- src/task.py
+++ src/task.py
@@ -3,17 +3,17 @@
 import time
 import schedule
 import add_soc
-import repo_update
-import soc_test
+# import repo_update
+# import soc_test
 
 
 def main_task():
     add_soc.main()
-    repo_update.main()
-    soc_test.main()
+    # repo_update.main()
+    # soc_test.main()
 
 
-schedule.every(3).minutes.do(main_task)
+schedule.every(1).minutes.do(main_task)
 
 while True:
     schedule.run_pending()
