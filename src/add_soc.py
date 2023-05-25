b056eb94f66890316c4d74457a577b0b67acee17
maksyuki <maksyuki@126.com>
2023-05-25 16:15:26 +0800

    feat: use class to rewrite the code

--- src/add_soc.py
+++ src/add_soc.py
@@ -6,45 +6,52 @@ from typing import List
 import cicd_config
 from data_type import CoreInfo
 
-# cores: [(bran, url, id)]
-cores = []
+
+class Cores(object):
+    def __init__(self):
+        self.cores = []
+
+    # 1. pattern: ysyx_([0-9]{6})
+    # 2. in id list
+    def check_valid(self, val: str) -> str:
+        if re.match('ysyx_[0-9]{8}', val) is not None:
+            return val
+        else:
+            return ''
+
+    def fill_data(self, term: List[str]):
+        self.cores.append(CoreInfo(cicd_config.CUR_BRAN, term[0], term[1]))
+
+    def handle_err(self, val: str):
+        # NOTE: need to write to the submit info
+        print('ID: error format, the err val: ' + val)
+
+    def add(self):
+        with open(cicd_config.SOC_LIST_PATH, 'r+', encoding='utf-8') as fp:
+            for v in fp.readlines():
+                tmp = v.split()
+                # print(tmp[1])
+                if self.check_valid(tmp[1]) != '':
+                    self.fill_data(tmp)
+                else:
+                    self.handle_err(tmp)
+
+    def update(self):
+        os.chdir(cicd_config.SUBMIT_DIR)
+        for v in self.cores:
+            print('git checkout ' + v.bran)
+            # os.system('git checkout ' + v.bran)
+            print('git submodule add ' + v.url + ' submit/' + v.sid)
+            # os.system('git submodule add ' + v.url + ' submit/' + v.sid)
 
 
-# 1. pattern: ysyx_([0-9]{6})
-# 2. in id list
-def checkValid(val: str) -> str:
-    if re.match('ysyx_[0-9]{8}', val) is not None:
-        return val
-    else:
-        return ''
-
-
-def fillData(term: List[str]):
-    cores.append(CoreInfo(cicd_config.CUR_BRAN, term[0], term[1]))
-
-
-def handleIDErr(val: str):
-    # need to write to the submit info
-    print('ID: error format, the err val: ' + val)
+cores = Cores()
 
 
 def main():
     print('[add soc]')
-    with open(cicd_config.SOC_LIST_PATH, 'r+', encoding='utf-8') as fp:
-        for v in fp.readlines():
-            tmp = v.split()
-            # print(tmp[1])
-            if checkValid(tmp[1]) != '':
-                fillData(tmp)
-            else:
-                handleIDErr(tmp)
-
-    os.chdir(cicd_config.SUBMIT_DIR)
-    for v in cores:
-        print('git checkout ' + v.bran)
-        # os.system('git checkout ' + v.bran)
-        print('git submodule add ' + v.url + ' submit/' + v.sid)
-        # os.system('git submodule add ' + v.url + ' submit/' + v.sid)
+    cores.add()
+    cores.update()
 
 
 if __name__ == '__main__':
--- src/data_type.py
+++ src/data_type.py
@@ -1,14 +1,7 @@
 #!/bin/python
 
-
 class CoreInfo(object):
     def __init__(self, bran: str, url: str, sid: str):
         self.bran = bran
         self.url = url
         self.sid = sid
-
-
-class CoreQueue(object):
-    def __init__(self):
-        self.val_list = []
-        self.id_list = []
