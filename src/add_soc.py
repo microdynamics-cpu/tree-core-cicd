cc936185348c9a1c40c1f4f52c4f12ecfa4c35cd
maksyuki <maksyuki@126.com>
2023-05-26 20:37:15 +0800

    feat: mv id list update from repo update module into add soc

--- src/add_soc.py
+++ src/add_soc.py
@@ -10,6 +10,11 @@ from data_type import CoreInfo
 class Cores(object):
     def __init__(self):
         self.cores = []
+        self.id_list = []
+
+    def clear(self):
+        self.cores.clear()
+        self.id_list.clear()
 
     # 1. pattern: ysyx_([0-9]{6})
     # 2. in id list
@@ -20,7 +25,7 @@ class Cores(object):
             return ''
 
     def fill_data(self, term: List[str]):
-        self.cores.append(CoreInfo(cicd_config.CUR_BRAN, term[0], term[1]))
+        self.cores.append(CoreInfo(term[0], term[1]))
 
     def handle_err(self, val: str):
         # NOTE: need to write to the submit info
@@ -36,13 +41,37 @@ class Cores(object):
                 else:
                     self.handle_err(tmp)
 
+    # update the id list
     def update(self):
         os.chdir(cicd_config.SUBMIT_DIR)
-        for v in self.cores:
-            print('git checkout ' + v.bran)
-            # os.system('git checkout ' + v.bran)
-            print('git submodule add ' + v.url + ' submit/' + v.sid)
-            # os.system('git submodule add ' + v.url + ' submit/' + v.sid)
+        # os.system('git checkout ' + cicd_config.CUR_BRAN)
+        print('git checkout ' + cicd_config.CUR_BRAN)
+        with open(cicd_config.ID_LIST_PATH, 'r+', encoding='utf-8') as fp:
+            for v in fp:
+                self.id_list.append(v.rstrip('\n'))
+
+        self.id_list.sort()
+        self.cores.sort(key=lambda v: v.sid)
+
+        new_id = []
+        for va in self.cores:
+            is_find = False
+            for vb in self.id_list:
+                if va.sid == vb:
+                    is_find = True
+                    break
+
+            if is_find is False:
+                # os.system('git submodule add ' + va.url + ' submit/' + va.sid)
+                print('git submodule add ' + va.url + ' submit/' + va.sid)
+                new_id.append(va.sid)
+
+        self.id_list += new_id
+        self.id_list.sort()
+        # print(self.id_list)
+        with open(cicd_config.ID_LIST_PATH, 'w+', encoding='utf-8') as fp:
+            for v in self.id_list:
+                fp.write(v + '\n')
 
 
 cores = Cores()
@@ -50,9 +79,10 @@ cores = Cores()
 
 def main():
     print('[add soc]')
+
+    cores.clear()
     cores.add()
     cores.update()
-
 
 if __name__ == '__main__':
     main()
