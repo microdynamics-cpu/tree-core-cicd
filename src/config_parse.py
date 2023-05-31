7733ce354ecb982a8c737c8c0182f190f547d05c
maksyuki <maksyuki@126.com>
2023-05-31 10:30:24 +0800

    feat: modify multi config parsers impl

--- src/config_parse.py
+++ src/config_parse.py
@@ -1,38 +1,84 @@
 import os
-import cicd_config
+from typing import Any, Dict
 import tomli
+import cicd_config
+from data_type import IvConfig, VerConfig, VcsConfig, DcConfig
 
 
-def vcs_config_parse():
-    pass
+class Config(object):
+    def __init__(self):
+        self.commit_info = ''
+        self.iv = IvConfig()
+        self.ver = VerConfig()
+        self.vcs = VcsConfig(False, 'all', 100)
+        self.dc = DcConfig(100)
+
+    def check_config(self, sid) -> bool:
+        repo_path = cicd_config.SUB_DIR + '/' + sid
+        # tmp = repo_path + '/def_config.toml'
+        tmp = 'def_config.toml'
+        if os.path.isfile(tmp):
+            with open(tmp, 'rb') as fp:
+                res = tomli.load(fp)
+                print(res)
+
+                cmd = 'git log origin/' + cicd_config.BRANCH_NAME_DEV
+                cmd += ' ' + repo_path
+                cmd += ' --pretty=format:"%s" -1'
+                print(cmd)
+                # commit_info = cicd_config.exec_cmd(cmd)
+                self.commit_info = 'vcs'
+                # print(res.keys())
+                std_config_keys = ['iv_config', 'ver_config', 'vcs_config']
+                is_valid = False
+                for v in std_config_keys:
+                    if v in res.keys(
+                    ) and res[v]['commit_info'] == self.commit_info:
+                        print('[read ' + v + ']')
+                        is_valid = True
+                        self.config_parse(res[v])
+                return is_valid
+        else:
+            return False
+
+    def iv_config_parse(self, config: Dict[str, Any]):
+        print(config)
+
+    def ver_config_parse(self, config: Dict[str, Any]):
+        print(config)
+
+    def vcs_config_parse(self, config: Dict[str, Any]):
+        # print(config)
+        self.vcs.wave = config['wave'] == 'on'
+        self.vcs.prog = config['prog']
+        self.vcs.freq = config['freq']
+        print(self.vcs)
+
+    def dc_config_parse(self, config: Dict[str, Any]):
+        print(config)
+        self.dc.freq = config['freq']
+
+    def config_parse(self, config: Dict[str, Any]):
+        if self.commit_info == 'iv':
+            self.iv_config_parse(config)
+        elif self.commit_info == 'ver':
+            self.ver_config_parse(config)
+        elif self.commit_info == 'vcs':
+            self.vcs_config_parse(config)
+        elif self.commit_info == 'dc':
+            self.dc_config_parse(config)
 
 
-def dc_config_parse():
-    pass
+def_config = Config()
 
 
 def main(sid: str):
-    repo_path = cicd_config.SUB_DIR + '/' + sid
-    # tmp = repo_path + '/def_config.toml'
-    tmp = 'def_config.toml'
-    if os.path.isfile(tmp):
-        with open(tmp, 'rb') as fp:
-            res = tomli.load(fp)
-            print(res)
-
-            cmd = 'git log origin/' + cicd_config.BRANCH_NAME_DEV
-            cmd += ' ' + repo_path
-            cmd += ' --pretty=format:"%s" -1'
-            print(cmd)
-            # commit_info = cicd_config.exec_cmd(cmd)
-            commit_info = 'vcs'
-            print(res.keys())
-            if commit_info in res.keys():
-                pass
-            else:
-                print('commit info is not found!')
+    res = def_config.check_config(sid)
+    if res:
+        pass
     else:
-        print('def_config.toml is not found!')
+        print('def_config.toml or commit info is not found!')
+
 
 
 if __name__ == '__main__':
