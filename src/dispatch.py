2eecea729155f18d8bdca808c1133f955d7a5208
maksyuki <maksyuki@126.com>
2023-05-31 10:42:14 +0800

    feat: add test tasks dispatch

--- src/dispatch.py
+++ src/dispatch.py
@@ -2,6 +2,10 @@
 
 import cicd_config
 import config_parse
+import iv_test
+import ver_test
+import vcs_test
+import dc_test
 
 
 def main():
@@ -9,9 +13,18 @@ def main():
     with open(cicd_config.QUEUE_LIST_PATH, 'r+', encoding='utf-8') as fp:
         cores = fp.readlines()
         if cores != []:
-            res = cores[0].split()
-            # print(res)
-            config_parse.main(res[0])
+            tmp = cores[0].split()
+            # print(tmp)
+            config_parse.main(tmp[0])
+            res = 'vcs'
+            if res == 'iv':
+                iv_test.main()
+            elif res == 'ver':
+                ver_test.main()
+            elif res == 'vcs':
+                vcs_test.main()
+            elif res == 'dc':
+                dc_test.main()
         else:
             print('this is not core in queue')
 
