e45ef6262cb0c3c6d7ebf10a903cb8479b3e7e1b
maksyuki <maksyuki@126.com>
2023-06-02 16:54:40 +0800

    feat: add vcs test class

--- src/vcs_test.py
+++ src/vcs_test.py
@@ -1,8 +1,28 @@
 #!/bin/python
+import os
+
+
+class VcsTest(object):
+    def __init__(self):
+        pass
+
+    def clear(self):
+        cmd = 'cd lib/vcs/run && make clean && '
+        cmd += 'rm temp.fp getReg* novas* verdi* -rf'
+        os.system(cmd)
+        cmd = 'find lib/vcs/cpu/* | grep -v ysyx_210000.v | xargs rm -rf'
+        os.system(cmd)
+
+    def create_dir(self):
+        pass
+
+
+vcstest = VcsTest()
 
 
 def main():
-    pass
+    print('[vcs test]')
+    vcstest.clear()
 
 
 if __name__ == '__main__':
